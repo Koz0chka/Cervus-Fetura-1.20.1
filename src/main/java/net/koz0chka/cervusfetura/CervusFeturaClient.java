ee36b4039b2d1be87592cc1770f707b96a2f93e4
koz0chka <foxy2004@list.ru>
2024-02-14 12:26:42 +0300

    1

---
+++ src/main/java/net/koz0chka/cervusfetura/CervusFeturaClient.java
@@ -0,0 +1,11 @@
+package net.koz0chka.cervusfetura;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CervusFeturaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
